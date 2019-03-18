9ecca99d1ae1f062947620db1920fdacb37f54fe
ironman <elvisbueno1010@gmail.com>
2019-03-17 21:35:16 -0500

    subtitle created

--- app/src/main/java/com/example/jarvis/JarvisMain.java
+++ app/src/main/java/com/example/jarvis/JarvisMain.java
@@ -1,10 +1,116 @@
 package com.example.jarvis;
 
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.EditText;
+import android.widget.Button;
+import android.util.Log;
+
+import java.util.Locale;
 
 
 public class JarvisMain extends AppCompatActivity {
+    private TextToSpeech mTTS;
+    private EditText mEditText;
+    private SeekBar mSeekBarPitch;
+    private SeekBar mSeekBarSpeed;
+    private Button mButtonSpeak;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jarvis_main);
+
+        mButtonSpeak = findViewById(R.id.button_speak);
+
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status == TextToSpeech.SUCCESS){
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("TTS", "Language not supported");
+                    }else{
+                        mButtonSpeak.setEnabled(true);
+                    }
+                }else{
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+        mEditText = findViewById(R.id.edit_text);
+        mSeekBarPitch = findViewById(R.id.seek_bar_pitch);
+        mSeekBarSpeed = findViewById(R.id.seek_bar_speed);
+
+        mButtonSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+    }
+
+    private void speak(){
+        String text = mEditText.getText().toString();
+        float pitch = (float) mSeekBarPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+
+        float speed = (float) mSeekBarSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+
+        mTTS.setPitch(pitch);
+        mTTS.setSpeechRate(speed);
+
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mTTS != null){
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+
+
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Exit App")
+                .setMessage("Are you sure you want to go back to menu")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //super.onBackPressed();
+                        //Or used finish();
+                        finish();
+
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+    }
+
+
+
+
 
 
 }
